608fff0a45bbb9b46911ecc4e0902d3b83d458b4
Zaid Akhter <zaidakhter1202@gmail.com>
2022-03-05 15:05:07 +0500

    new api func

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,13 @@
 export default function handler(req, res) {
   res.status(200).json({
-    message: "Hi, Zaid here! Welcome to my conrner of the internet.",
-    links: [],
+    message: "Hi, Zaid here! Welcome to my corner of the internet.",
+    links: {
+      email: "zaidakhter1202@gmail.com",
+      github: "https://github.com/zaidakhterr",
+      linkedin: "https://www.linkedin.com/in/zaidakhterr/",
+      twitter: "https://twitter.com/zaidakhterr",
+      upwork: "https://www.upwork.com/freelancers/~01c3f1d9695773d1f0",
+      feedback: "https://forms.gle/pVdALKuhwjLskJLx8",
+    },
   });
 }
