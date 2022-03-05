6c5cca75e37a5ea1be621877bef86525af750686
Zaid Akhter <zaidakhter1202@gmail.com>
2022-03-05 13:33:42 +0500

    meta tags and stuff

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,52 @@ export default function Home() {
   return (
     <>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000" />
+
         <title>Zaid Akhter</title>
         <meta
           name="description"
           content="Zaid Akhter's corner of the internet."
         />
-        <link rel="icon" href="/favicon.ico" />
+
+        <meta itemProp="name" content="Zaid Akhter" />
+        <meta
+          itemProp="description"
+          content="Zaid Akhter's corner of the internet."
+        />
+        <meta itemProp="image" content="https://zaidakhter.com/card.png" />
+
+        <meta property="og:url" content="https://zaidakhter.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Zaid Akhter" />
+        <meta
+          property="og:description"
+          content="Zaid Akhter's corner of the internet."
+        />
+        <meta property="og:image" content="https://zaidakhter.com/card.png" />
+
+        <meta property="twitter:url" content="https://zaidakhter.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Zaid Akhter" />
+        <meta
+          name="twitter:description"
+          content="Zaid Akhter's corner of the internet."
+        />
+        <meta name="twitter:image" content="https://zaidakhter.com/card.png" />
       </Head>
       <div className="header">
         <div className="gradient" />
       </div>
       <div className="content">
-        <Image src="/zaid.png" alt="Zaid Akhter" width="120px" height="120px" />
+        <Image
+          src="/zaid.png"
+          alt="Zaid Akhter"
+          width="120px"
+          height="120px"
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAABtJREFUGFdj9Pf3///u3TsGRltb2/9v3rxhAABQrwkzPevuYAAAAABJRU5ErkJggg=="
+        />
         <h1>Zaid Akhter</h1>
         <p className="sub">
           <a href="https://en.wikipedia.org/wiki/Karachi">The City of Lights</a>
