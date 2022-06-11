3d227d94c60ffb055210d09fdb963aedace5d9f5
Zaid Akhter <zaidakhter1202@gmail.com>
2022-06-11 12:51:11 +0500

    update upwork url

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     message: "Hi, Zaid here! Welcome to my corner of the internet.",
     links: {
       email: "zaidakhter1202@gmail.com",
-      upwork: "https://www.upwork.com/freelancers/zaidakhter",
+      upwork: "https://www.upwork.com/freelancers/zaid",
       github: "https://github.com/zaidakhterr",
       linkedin: "https://www.linkedin.com/in/zaidakhterr/",
       twitter: "https://twitter.com/zaidakhterr",
